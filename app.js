a9a362046496f4af7ce7bf3146e22b7f23cf84bc
Sumya <sumyabazarbayarkhuu93@gmail.com>
2022-12-07 22:28:26 +0800

    LASt

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var roundScore = 0;
 var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 
-document.getElementById('#score-0').textContent = 0;
-document.getElementById('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
 
-document.getElementById("#current-0").textContent = 0;
-document.getElementById("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
 document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", shooShid);
+
+function shooShid() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    alert('shoo buulaa : ' + diceNumber);
+}
